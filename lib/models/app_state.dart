a375b4a642898d03f5eb453ad8730312af9db7d5
Timi Petre <gaxxis@yahoo.com>
2021-11-30 22:26:37 +0200

    Starting Project

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -1,1 +1,23 @@
-class AppState {}
+class AppState {
+  AppState({
+    this.titles = const <String>[],
+    this.isLoading = false,
+    this.page = 1,
+  });
+
+  final List<String> titles;
+  final bool isLoading;
+  final int page;
+
+  AppState copyWith({
+    final List<String>? titles,
+    final bool? isLoading,
+    final int? page,
+  }) {
+    return AppState(
+      titles: titles ?? this.titles,
+      isLoading: isLoading ?? this.isLoading,
+      page: page ?? this.page,
+    );
+  }
+}
